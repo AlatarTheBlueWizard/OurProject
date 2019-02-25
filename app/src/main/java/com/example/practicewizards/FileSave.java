9e90efa5836aa9805e55c31635a943c8bafb1845
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-02-25 10:24:20 -0700

    Edited file save class, still needs work

--- app/src/main/java/com/example/practicewizards/FileSave.java
+++ app/src/main/java/com/example/practicewizards/FileSave.java
@@ -1,15 +1,44 @@
 package com.example.practicewizards;
 
-import android.content.Context;
-import android.graphics.Bitmap;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
 
-import java.time.chrono.ThaiBuddhistEra;
+import java.io.File;
+import java.net.URI;
+
+import static android.content.ContentValues.TAG;
 
 public class FileSave {
-   public void SaveImage(Context context, Bitmap ImageToSave) {
-       Context ThePic = context;
-       String file_path = Environment.getExternalStorageDirectory().getAbsolutePath();//+NameOfFolder;
-       String CurrentDateAndTime = getCurrentDateAndTime();
-   }
+    private URI imageUri;
+
+    Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+    File photo;
+
+    try {
+        //place to store the picture that was taken
+        photo = this.createTemporaryFile("picture", ".jpg");
+        photo.delete();
+    } catch(Exception e) {
+        Log.v(TAG, "Can't create file to take the picture");
+        Toast.makeText(activity, "Please check SD card! Image shot is impossible!", 10000);
+    }
+
+    imageUri = Uri.fromFile(photo);
+    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+
+    //start camera intent
+    activity.startActivityForResult(this, intent, MenuShootImage);
+
+    private File createTemporaryFile(String part, String ext) throws Exception {
+        File tempDir = Environment.getExternalStorageDirectory();
+        tempDir = new File(tempDir.getAbsolutePath()+"/.temp/");
+        if(!tempDir.exists()) {
+            tempDir.mkdirs();
+        }
+        return File.createTempFile(part, ext, tempDir);
+    }
 }
