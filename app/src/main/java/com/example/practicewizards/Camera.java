6896b8da6be8bb88f83204f542dc47aa947a4dc9
benjaminhansen <benjamminhansen@gmail.com>
2019-02-25 12:03:14 -0700

    Updated private data needed

--- app/src/main/java/com/example/practicewizards/Camera.java
+++ app/src/main/java/com/example/practicewizards/Camera.java
@@ -1,18 +1,9 @@
 package com.example.practicewizards;
 
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CaptureRequest;
-import android.hardware.camera2.TotalCaptureResult;
-import android.hardware.camera2.params.InputConfiguration;
-import android.hardware.camera2.params.OutputConfiguration;
 import android.os.Build;
-import android.os.Handler;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
-import android.view.Surface;
+
 
 import java.util.List;
 
@@ -20,62 +11,19 @@ import java.util.List;
  * Class holding a single cameraDevice personalized for us to use and manipulate
  */
 public class Camera {
+    // Tag
+    private static final String TAG = "pracitcewizards.Camera";
+    // Camera ID
+    private String cameraId;
     // Camera object
     public CameraDevice cameraDevice;
+
+
 
     /**
      * Constructor for a Camera
      */
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public Camera() {
-        cameraDevice = new CameraDevice() {
-            @NonNull
-            @Override
-            public String getId() {
-                return null;
-            }
-
-            @Override
-            public void createCaptureSession(@NonNull List<Surface> outputs, @NonNull CameraCaptureSession.StateCallback callback, @Nullable Handler handler) throws CameraAccessException {
-
-            }
-
-            @Override
-            public void createCaptureSessionByOutputConfigurations(List<OutputConfiguration> outputConfigurations, CameraCaptureSession.StateCallback callback, @Nullable Handler handler) throws CameraAccessException {
-
-            }
-
-            @Override
-            public void createReprocessableCaptureSession(@NonNull InputConfiguration inputConfig, @NonNull List<Surface> outputs, @NonNull CameraCaptureSession.StateCallback callback, @Nullable Handler handler) throws CameraAccessException {
-
-            }
-
-            @Override
-            public void createReprocessableCaptureSessionByConfigurations(@NonNull InputConfiguration inputConfig, @NonNull List<OutputConfiguration> outputs, @NonNull CameraCaptureSession.StateCallback callback, @androidx.annotation.Nullable Handler handler) throws CameraAccessException {
-
-            }
-
-            @Override
-            public void createConstrainedHighSpeedCaptureSession(@androidx.annotation.NonNull List<Surface> outputs, @NonNull CameraCaptureSession.StateCallback callback, @Nullable Handler handler) throws CameraAccessException {
-
-            }
-
-            @androidx.annotation.NonNull
-            @Override
-            public CaptureRequest.Builder createCaptureRequest(int templateType) throws CameraAccessException {
-                return null;
-            }
-
-            @androidx.annotation.NonNull
-            @Override
-            public CaptureRequest.Builder createReprocessCaptureRequest(@NonNull TotalCaptureResult inputResult) throws CameraAccessException {
-                return null;
-            }
-
-            @Override
-            public void close() {
-
-            }
-        };
     }
 }
