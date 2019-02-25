0fd2f21d71ffe8236e37143b15a4f6fca83fe0de
Brian_064 <jarelbrian064@gmail.com>
2019-02-25 10:00:52 -0700

    Initial UI

--- app/src/main/java/com/example/practicewizards/MainActivity.java
+++ app/src/main/java/com/example/practicewizards/MainActivity.java
@@ -1,17 +1,58 @@
 package com.example.practicewizards;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView myImage;
+    ImageView myImage2;
+    Button button;
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myImage = (ImageView)findViewById(R.id.myImage);
+        myImage2 = (ImageView)findViewById(R.id.myImage2);
+
+        button = (Button)findViewById(R.id.button);
+        button2 = (Button)findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture();
+            }
+        });
     }
-    //Resolved merge conflict
-    // Benjamin
-    //Brian
+
+    public void takePicture() {
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, 0);
+    }
+
+    public void takePicture2() {
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK) {
+            Bitmap b = (Bitmap)data.getExtras().get("data");
+            myImage.setImageBitmap(b);
+        }
+    }
 
 }
