f3143d721bec2f7d89064406881a7a05e4ab75cf
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-02-27 15:33:07 -0700

    Edited Android manifest for file paths and permissions, also edited the saveFile class.

--- app/src/main/java/com/example/practicewizards/FileSave.java
+++ app/src/main/java/com/example/practicewizards/FileSave.java
@@ -1,5 +1,90 @@
 package com.example.practicewizards;
 
-public class FileSave {
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FileSave {
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        //Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  //prefix
+                ".jpg",   //suffix
+                storageDir      //directory
+        );
+
+        //save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //ensure that there is a camera activity to handle the intent
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            //create file where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                //error occurred
+            }
+            //continue only if the File was successfully created
+            if(photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                                                     "com.example.android.fileprovider",
+                                                      photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private void setPic() {
+        //get the dimensions of the view
+        int targetW = imageView.getWidth();
+        int targetH = imageView.getHeight();
+
+        //get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(currentPhotoPath, boptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        //determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        //decode the image file into a bitmap sized to fill the view
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, boptions);
+        imageView.setImageBitmap(bitmap);
+    }
 }
