d43fd219fabee3bdd06993cbee14a6206a549ea6
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-03-04 14:48:17 -0700

    Added photo capture class, will take photo and save it to the gallery

--- app/src/main/java/com/example/practicewizards/MainActivity.java
+++ app/src/main/java/com/example/practicewizards/MainActivity.java
@@ -2,22 +2,16 @@ package com.example.practicewizards;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
-import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST=1888;
     ImageView myImage;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,23 +25,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void takePicture() {
         Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        String pictureName = getPictureName();
-        File imagefile = new File(pictureName);
-        Uri pictureUri = Uri.fromFile(imagefile);
-        i.putExtra(MediaStore.EXTRA_OUTPUT,pictureUri);
+        //MediaStore.Images.Media.insertImage(getContentResolver(), yourBitmap, yourTitle , yourDescription);
         startActivityForResult(i, CAMERA_REQUEST);
     }
 
     public void takePicture2() {
         Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(i, 0);
-    }
-
-    private String getPictureName() {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
-        String timestamp = sdf.format(new Date());
-        return "Plane place image" + timestamp + ".jpg";
     }
 
     protected  void onActivityResult(int requestCode, int resultCode, Intent data) {
