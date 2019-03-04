1476c22b57e8673fb152a7ccabc850961396c36f
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-03-04 09:17:54 -0700

    Edited main

--- app/src/main/java/com/example/practicewizards/MainActivity.java
+++ app/src/main/java/com/example/practicewizards/MainActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
@@ -18,27 +16,11 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST=1888;
     ImageView myImage;
-    ImageView myImage2;
-    Button button;
-    Button button2;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        myImage = (ImageView)findViewById(R.id.myImage);
-        myImage2 = (ImageView)findViewById(R.id.myImage2);
-
-        button = (Button)findViewById(R.id.button);
-        button2 = (Button)findViewById(R.id.button2);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                takePicture();
-            }
-        });
     }
 
     public void takePicture() {
