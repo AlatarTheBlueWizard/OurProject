6442e7c282028b1a9b09abe7c13fedc49bebb7d9
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-03-06 14:50:16 -0700

    Added methods in main to save the image from the file directory gallery on the phone, also added a method to add the image to the gallery.

--- app/src/main/java/com/example/practicewizards/MainActivity.java
+++ app/src/main/java/com/example/practicewizards/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.practicewizards;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.SurfaceTexture;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST=1888;
@@ -65,7 +71,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void takePicture() {
         Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        //MediaStore.Images.Media.insertImage(getContentResolver(), yourBitmap, yourTitle , yourDescription);
         startActivityForResult(i, CAMERA_REQUEST);
     }
 
@@ -84,4 +89,23 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //creates the image file with date format
+    public static File createImageFile() throws IOException {
+        //create image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(System.currentTimeMillis());
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) + "/Camera/");
+        if(!storageDir.exists())
+            storageDir.mkdirs();
+        File image = File.createTempFile(timeStamp, ".jpeg", storageDir);
+        return image;
+    }
+
+    //Saves the picture to the gallery
+    public static void addPicToGallery(Context context, String photoPath) {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(photoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        context.sendBroadcast(mediaScanIntent);
+    }
 }
